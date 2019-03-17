596358c15c11d11521a4ca7dae0e21afb533fb8b
Amos Wong <amoswwj@gmail.com>
2019-03-17 23:32:39 +0800

    Add server for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,19 @@
 import typescript from 'rollup-plugin-typescript';
+import serve from 'rollup-plugin-serve';
+
+const mode = process.env.NODE_ENV === 'production'  ? 'production' : 'development';
 
 export default {
   input: './src/index.ts',
   plugins: [
     typescript(),
+    ...(mode === 'production' ? [] : [serve({
+      open: true,
+      openPage: '/example',
+      contentBase: '.',
+      host: 'localhost',
+      port: 8080,
+    })]),
   ],
   output: [
     {
